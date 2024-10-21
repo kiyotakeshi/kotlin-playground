536951fdbe1893885e33d0b2091d3f26eafa2578
takeshi-kiyota <takeshi-kiyota@bm-sms.co.jp>
2024-10-21 11:44:37 +0900

    forEach と例外の動きを確認

---
+++ src/test/kotlin/com/kiyotakeshi/playground/ForEachAndException.kt
@@ -0,0 +1,26 @@
+package com.kiyotakeshi.playground
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+class ForEachAndException {
+
+    @Disabled
+    @Test
+    fun `test missing IDs throw InternalException`() {
+        val map = mapOf(1 to "one", 3 to "three", 5 to "five")
+        val ids = listOf(1, 2, 3, 4, 5)
+
+        ids.forEach { id ->
+            println("execute $id")
+            // 例外を出した時点で処理が終わる = execute 3 は出力されない
+            map[id] ?: internalError("id $id not found")
+        }
+    }
+}
+
+private fun internalError(message: String?, cause: Throwable? = null): Nothing {
+    throw InternalException(message, cause)
+}
+
+class InternalException(message: String?, cause: Throwable? = null) : RuntimeException(message, cause)
